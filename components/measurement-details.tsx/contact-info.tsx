90f3226bb3f6dddaa3a2a2ed185198cb280e7c1e
Alejandro Garcia <josealegar19@gmail.com>
2026-03-24 12:53:42 -0700

    contactinfo adjustments, still not done with it

--- components/measurement-details.tsx/contact-info.tsx
+++ components/measurement-details.tsx/contact-info.tsx
@@ -1,44 +1,51 @@
 import { fonts } from "@/theme/fonts";
+import { ContactInfoType } from "@/types/measurementInfo";
 import { useRouter } from "expo-router";
 import { Text, View } from "react-native";
 import { EditableCard } from "./editable-card";
 
 export const ContactInfo = ({}) => {
   const router = useRouter();
-  const contactInfo = {
-    name: "Jose Alejandro Garcia Figueroa",
-    address: "1234 Main Street, Springfield, IL 62704",
+  const contactInfo: ContactInfoType = {
+    name: "John Doe",
+    address: "123 Main St, Anytown, USA",
     email: "josealegar19@gmail.com",
-    phone: "(619) 490-8254",
-    date: "12/12/2026",
+    phone: "619-555-1234",
+    date: "2023-10-01",
   } as const;
   return (
     <EditableCard
       title={"Contact Info"}
       onPress={() => router.push("/measurement/new/contact-info-form")}
     >
-      <View>
-        <Text style={{ fontFamily: fonts.regular }}>
-          <Text style={{ fontFamily: fonts.bold }}>Name: </Text>
-          {contactInfo.name}
+      {Object.keys(contactInfo).length === 0 ? (
+        <Text style={{ fontFamily: fonts.regular, fontStyle: "italic" }}>
+          No contact information provided. Tap to add.
         </Text>
-        <Text style={{ fontFamily: fonts.regular }}>
-          <Text style={{ fontFamily: fonts.bold }}>Address: </Text>
-          {contactInfo.address}
-        </Text>
-        <Text style={{ fontFamily: fonts.regular }}>
-          <Text style={{ fontFamily: fonts.bold }}>Email: </Text>
-          {contactInfo.email}
-        </Text>
-        <Text style={{ fontFamily: fonts.regular }}>
-          <Text style={{ fontFamily: fonts.bold }}>Phone: </Text>
-          {contactInfo.phone}
-        </Text>
-        <Text style={{ fontFamily: fonts.regular }}>
-          <Text style={{ fontFamily: fonts.bold }}>Date: </Text>
-          {contactInfo.date}
-        </Text>
-      </View>
+      ) : (
+        <View>
+          <Text style={{ fontFamily: fonts.regular }}>
+            <Text style={{ fontFamily: fonts.bold }}>Name: </Text>
+            {contactInfo.name}
+          </Text>
+          <Text style={{ fontFamily: fonts.regular }}>
+            <Text style={{ fontFamily: fonts.bold }}>Address: </Text>
+            {contactInfo.address}
+          </Text>
+          <Text style={{ fontFamily: fonts.regular }}>
+            <Text style={{ fontFamily: fonts.bold }}>Email: </Text>
+            {contactInfo.email}
+          </Text>
+          <Text style={{ fontFamily: fonts.regular }}>
+            <Text style={{ fontFamily: fonts.bold }}>Phone: </Text>
+            {contactInfo.phone}
+          </Text>
+          <Text style={{ fontFamily: fonts.regular }}>
+            <Text style={{ fontFamily: fonts.bold }}>Date: </Text>
+            {contactInfo.date}
+          </Text>
+        </View>
+      )}
     </EditableCard>
   );
 };
