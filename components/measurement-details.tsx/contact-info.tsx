21e123c178d864331c2876d296d618cf4f9d56a1
Alejandro Garcia <josealegar19@gmail.com>
2026-03-27 15:36:09 -0700

    Contact Info empty state

--- components/measurement-details.tsx/contact-info.tsx
+++ components/measurement-details.tsx/contact-info.tsx
@@ -6,19 +6,13 @@ import { EditableCard } from "./editable-card";
 
 export const ContactInfo = ({}) => {
   const router = useRouter();
-  const contactInfo: ContactInfoType = {
-    name: "John Doe",
-    address: "123 Main St, Anytown, USA",
-    email: "josealegar19@gmail.com",
-    phone: "619-555-1234",
-    date: "2023-10-01",
-  } as const;
+  const contactInfo = null as ContactInfoType | null;
   return (
     <EditableCard
       title={"Contact Info"}
       onPress={() => router.push("/measurement/new/contact-info-form")}
     >
-      {Object.keys(contactInfo).length === 0 ? (
+      {!contactInfo ? (
         <Text style={{ fontFamily: fonts.regular, fontStyle: "italic" }}>
           No contact information provided. Tap to add.
         </Text>
@@ -32,14 +26,18 @@ export const ContactInfo = ({}) => {
             <Text style={{ fontFamily: fonts.bold }}>Address: </Text>
             {contactInfo.address}
           </Text>
-          <Text style={{ fontFamily: fonts.regular }}>
-            <Text style={{ fontFamily: fonts.bold }}>Email: </Text>
-            {contactInfo.email}
-          </Text>
-          <Text style={{ fontFamily: fonts.regular }}>
-            <Text style={{ fontFamily: fonts.bold }}>Phone: </Text>
-            {contactInfo.phone}
-          </Text>
+          {contactInfo.email && (
+            <Text style={{ fontFamily: fonts.regular }}>
+              <Text style={{ fontFamily: fonts.bold }}>Email: </Text>
+              {contactInfo.email}
+            </Text>
+          )}
+          {contactInfo.phone && (
+            <Text style={{ fontFamily: fonts.regular }}>
+              <Text style={{ fontFamily: fonts.bold }}>Phone: </Text>
+              {contactInfo.phone}
+            </Text>
+          )}
           <Text style={{ fontFamily: fonts.regular }}>
             <Text style={{ fontFamily: fonts.bold }}>Date: </Text>
             {contactInfo.date}
