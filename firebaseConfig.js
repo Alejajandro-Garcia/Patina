6a92e392a13d48f4a4f95c292a64412b78c36f1b
Alejandro Garcia <josealegar19@gmail.com>
2026-04-02 11:16:17 -0700

    getting the auth and db from firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,7 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -13,5 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, analytics };
+export { analytics, app, auth, db };
