b0c368e2e11ecd7de4bf34ee8899abebfe29e6ae
Alejandro Garcia <josealegar19@gmail.com>
2026-05-06 15:02:00 -0700

    adding to new-measurement that has confirmation modal for on go back

--- app/measurement/new-measurement.tsx
+++ app/measurement/new-measurement.tsx
@@ -1,10 +1,31 @@
+import { ConfirmationModal } from "@/components/confirmation-modal";
 import { MeasurementDetails } from "@/components/measurement-details/measurement-details";
 import { PatinaPage } from "@/components/patina-page";
+import useMeasurementDetailsStore from "@/stores/use-measurement-details-store";
+import { useRouter } from "expo-router";
+import { useState } from "react";
 
 export default function NewMeasurement() {
+  const router = useRouter();
+  const reset = useMeasurementDetailsStore((state) => state.reset);
+  const [unsavedVisible, setUnsavedVisible] = useState(false);
+
   return (
-    <PatinaPage>
-      <MeasurementDetails measurement={null} />
-    </PatinaPage>
+    <>
+      <ConfirmationModal
+        visible={unsavedVisible}
+        title="Discard changes?"
+        message="Any unsaved changes will be discarded."
+        onClose={() => setUnsavedVisible(false)}
+        onConfirm={() => {
+          setUnsavedVisible(false);
+          reset();
+          router.back();
+        }}
+      />
+      <PatinaPage goBackCallBack={() => setUnsavedVisible(true)}>
+        <MeasurementDetails measurement={null} />
+      </PatinaPage>
+    </>
   );
 }
