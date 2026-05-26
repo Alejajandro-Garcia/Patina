9a64a4a9308b7f040cce47ff71ab2c8e2f11604e
Alejandro Garcia <josealegar19@gmail.com>
2026-05-26 12:34:01 -0700

    metric not having width be spaced with meters

--- helpers/format-units.ts
+++ helpers/format-units.ts
@@ -4,7 +4,7 @@ import { measurementUnitTruncated, METRIC, Units } from "@/types/units";
 export const formatAreaString = (area: AreaType, units: Units) => {
   const stepsString = area.steps ? ` x ${area.steps}` : "";
   if (units === METRIC) {
-    return `${area.length} ${measurementUnitTruncated[units]} x ${area.width}${stepsString}`;
+    return `${area.length} ${measurementUnitTruncated[units]} x ${area.width} ${measurementUnitTruncated[units]}${stepsString}`;
   }
   return `${area.length}${measurementUnitTruncated[units]} x ${area.width}${measurementUnitTruncated[units]}${stepsString}`;
 };
