bc5f753e9db133af3e68e560d2b07376f5c8c534
Alejandro Garcia <josealegar19@gmail.com>
2026-06-25 20:58:53 -0700

    adding percentage to hook

--- hooks/use-calculate-area.tsx
+++ hooks/use-calculate-area.tsx
@@ -1,8 +1,4 @@
-import {
-  roundSqFt,
-  sqFtToSqM,
-  sqMToSqFt,
-} from "@/helpers/area-calculations";
+import { roundSqFt, sqFtToSqM, sqMToSqFt } from "@/helpers/area-calculations";
 import { IMPERIAL, Units } from "@/types/units";
 import MeasurementModel from "@/watermelonDB/model/measurement";
 import { useMemo } from "react";
@@ -10,22 +6,27 @@ import { useMemo } from "react";
 interface useCalculateLandingPageAreaProps {
   measurement: MeasurementModel;
   units: Units;
+  percentage: number;
 }
 
 const useCalculateLandingPageArea = ({
   measurement,
   units,
+  percentage,
 }: useCalculateLandingPageAreaProps) => {
   return useMemo(() => {
+    let total = 0;
     const currentlyImperial = units === IMPERIAL;
-    if (measurement.imperial === currentlyImperial) return measurement.total;
+    if (measurement.imperial === currentlyImperial) {
+      total = measurement.total;
+      return roundSqFt(total + total * percentage);
+    }
 
-    return roundSqFt(
-      currentlyImperial
-        ? sqMToSqFt(measurement.total)
-        : sqFtToSqM(measurement.total),
-    );
-  }, [measurement, units]);
+    total = currentlyImperial
+      ? sqMToSqFt(measurement.total)
+      : sqFtToSqM(measurement.total);
+    return roundSqFt(total + total * percentage);
+  }, [measurement, units, percentage]);
 };
 
 export default useCalculateLandingPageArea;
