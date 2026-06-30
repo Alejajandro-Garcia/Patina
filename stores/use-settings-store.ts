c7a2668526003e76e9d942c709ec2ee3132349d4
Alejandro Garcia <josealegar19@gmail.com>
2026-06-29 18:07:51 -0700

    persist beyond closing

--- stores/use-settings-store.ts
+++ stores/use-settings-store.ts
@@ -1,7 +1,9 @@
 import { Units } from "@/types/units";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 interface SettingsState {
-  units: "Imperial (sqft/in)" | "Metric (m/cm)";
+  units: Units;
   percentage: number;
   setUnits: (units: Units) => void;
   setPercentage: (percentage: number) => void;
@@ -12,10 +14,18 @@ const initialState: Pick<SettingsState, "units" | "percentage"> = {
   percentage: 0,
 };
 
-const useSettingsStore = create<SettingsState>((set) => ({
-  ...initialState,
-  setUnits: (units: Units) => set({ units }),
-  setPercentage: (percentage: number) => set({ percentage }),
-}));
+const useSettingsStore = create<SettingsState>()(
+  persist(
+    (set) => ({
+      ...initialState,
+      setUnits: (units: Units) => set({ units }),
+      setPercentage: (percentage: number) => set({ percentage }),
+    }),
+    {
+      name: "settings-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
 
 export default useSettingsStore;
