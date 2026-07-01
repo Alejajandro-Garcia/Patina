8bc1f34828aa0055666f339656b0d9e62409ea22
Alejandro Garcia <josealegar19@gmail.com>
2026-07-01 13:33:56 -0700

    allow debug and error statements, but everything to console is an error

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,21 @@ module.exports = defineConfig([
   {
     ignores: ["dist/*"],
     rules: {
-      "no-console": "error",
+      // console.log/info/warn are errors; debug/error are downgraded to warnings below
+      "no-console": ["error", { allow: ["debug", "error"] }],
+      "no-restricted-syntax": [
+        "warn",
+        {
+          selector:
+            "CallExpression[callee.object.name='console'][callee.property.name='debug']",
+          message: "Unexpected console.debug statement.",
+        },
+        {
+          selector:
+            "CallExpression[callee.object.name='console'][callee.property.name='error']",
+          message: "Unexpected console.error statement.",
+        },
+      ],
       "no-var": "error",
     },
   },
